dd2badc3c558c5faf643fc8d5b3d69c5bcac430e
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2025-01-31 14:05:30 +0800

    updated host

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def endpoint_not_found(_error):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    host = os.environ.get("HOST", '127.0.0.1')
+    app.run(host=host, port=port)
